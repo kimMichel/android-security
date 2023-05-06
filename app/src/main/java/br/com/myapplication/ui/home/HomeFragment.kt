63d4d6bc67c2b679392ad38e1c1d6dde67b25f19
Michel Kim <kim98.michel@gmail.com>
2023-05-06 13:23:51 -0300

    add home fragment

---
+++ app/src/main/java/br/com/myapplication/ui/home/HomeFragment.kt
@@ -0,0 +1,21 @@
+package br.com.myapplication.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.myapplication.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
