4e288a01f80428f9a13b020b93ff4f6d873c7053
Michel Kim <kim98.michel@gmail.com>
2023-05-11 23:06:02 -0300

    fix encryption and decryption logic

--- app/src/main/java/br/com/myapplication/shared/security/CryptoManager.kt
+++ app/src/main/java/br/com/myapplication/shared/security/CryptoManager.kt
@@ -7,19 +7,22 @@ import java.security.KeyStore
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
 object CryptoManager {
 
     private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
     private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
-    private const val PADDING = KeyProperties.ENCRYPTION_PADDING_NONE
+    private const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
     private const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
     private const val ALIAS = "myApplicationKeys"
     private const val ANDROID_KEYSTORE = "AndroidKeyStore"
 
-    private val ks: KeyStore = KeyStore.getInstance(ALIAS).apply {
+    private val ks: KeyStore = KeyStore.getInstance(ANDROID_KEYSTORE).apply {
         load(null)
     }
+
+    private var ivParameter: ByteArray? = null
 
     private fun getKey(): SecretKey {
         val existingKey = ks.getEntry(ALIAS, null) as? KeyStore.SecretKeyEntry
@@ -31,7 +34,6 @@ object CryptoManager {
             ALIAS,
             KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
         ).run {
-            setDigests(KeyProperties.DIGEST_SHA256)
             setBlockModes(BLOCK_MODE)
             setEncryptionPaddings(PADDING)
             setUserAuthenticationRequired(false)
@@ -43,10 +45,11 @@ object CryptoManager {
     }
 
     fun encrypt(data: String): String {
-        val byteArray: ByteArray = Base64.decode(data, Base64.DEFAULT)
+        val byteArray: ByteArray = Base64.decode(data.trim(), Base64.DEFAULT)
         val cipher = Cipher.getInstance(TRANSFORMATION).apply {
             init(Cipher.ENCRYPT_MODE, getKey())
         }
+        ivParameter = cipher.iv
         val cipherData = cipher.doFinal(byteArray)
         return Base64.encodeToString(cipherData, Base64.DEFAULT)
     }
@@ -54,9 +57,10 @@ object CryptoManager {
     fun decrypt(data: String): String {
         val byteArray: ByteArray = Base64.decode(data, Base64.DEFAULT)
         val cipher = Cipher.getInstance(TRANSFORMATION).apply {
-            init(Cipher.DECRYPT_MODE, getKey())
+            init(Cipher.DECRYPT_MODE, getKey(), IvParameterSpec(ivParameter))
         }
-        return String(cipher.doFinal(byteArray), Charsets.UTF_8)
+        val cipherData = cipher.doFinal(byteArray)
+        return Base64.encodeToString(cipherData, Base64.DEFAULT)
     }
 
 }
