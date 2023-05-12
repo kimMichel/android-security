bdcd92a6758a17797c8e3674a4950f4af4faa591
Michel Kim <kim98.michel@gmail.com>
2023-05-11 21:05:00 -0300

    fix encryption and decrypt functions and change algorithm of rsa to aes

--- app/src/main/java/br/com/myapplication/shared/security/CryptoManager.kt
+++ app/src/main/java/br/com/myapplication/shared/security/CryptoManager.kt
@@ -3,79 +3,60 @@ package br.com.myapplication.shared.security
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import android.util.Base64
-import java.math.BigInteger
-import java.security.KeyPair
-import java.security.KeyPairGenerator
 import java.security.KeyStore
-import java.security.PrivateKey
-import java.security.PublicKey
 import javax.crypto.Cipher
-import javax.security.auth.x500.X500Principal
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 
 object CryptoManager {
 
-    private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_RSA
-    private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_ECB
-    private const val PADDING = KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1
+    private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
+    private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
+    private const val PADDING = KeyProperties.ENCRYPTION_PADDING_NONE
     private const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
-    private const val KEY_SIZE = 2048
     private const val ALIAS = "myApplicationKeys"
     private const val ANDROID_KEYSTORE = "AndroidKeyStore"
 
-    private fun getPublicKey(): PublicKey {
-        return try {
-            val keyStore: KeyStore = KeyStore.getInstance(ANDROID_KEYSTORE).apply {
-                load(null)
-            }
-            keyStore.getCertificate(ALIAS).publicKey
-        } catch (e: NullPointerException) {
-            generateKey().public
-        }
+    private val ks: KeyStore = KeyStore.getInstance(ALIAS).apply {
+        load(null)
     }
 
-    private fun getPrivateKey(): PrivateKey {
-        val keyStore: KeyStore = KeyStore.getInstance(ANDROID_KEYSTORE).apply {
-            load(null)
-        }
-        return keyStore.getKey(ALIAS, null) as PrivateKey
+    private fun getKey(): SecretKey {
+        val existingKey = ks.getEntry(ALIAS, null) as? KeyStore.SecretKeyEntry
+        return existingKey?.secretKey ?: generateKey()
     }
-
-    private fun generateKey(): KeyPair {
-        val keyPairGenerator: KeyPairGenerator = KeyPairGenerator.getInstance(ALGORITHM, ANDROID_KEYSTORE)
-
-        val parameterSpec: KeyGenParameterSpec = KeyGenParameterSpec.Builder(
+    private fun generateKey(): SecretKey {
+        val kg: KeyGenerator = KeyGenerator.getInstance(ALGORITHM, ANDROID_KEYSTORE)
+        val kps: KeyGenParameterSpec = KeyGenParameterSpec.Builder(
             ALIAS,
             KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
         ).run {
-            setCertificateSerialNumber(BigInteger.valueOf(777))
-            setCertificateSubject(X500Principal("CN=$ALIAS"))
-            setKeySize(KEY_SIZE)
+            setDigests(KeyProperties.DIGEST_SHA256)
             setBlockModes(BLOCK_MODE)
             setEncryptionPaddings(PADDING)
-            setDigests(KeyProperties.DIGEST_SHA256)
             setUserAuthenticationRequired(false)
+            setRandomizedEncryptionRequired(true)
             build()
         }
-        keyPairGenerator.initialize(parameterSpec)
-        return keyPairGenerator.genKeyPair()
+        kg.init(kps)
+        return kg.generateKey()
     }
 
     fun encrypt(data: String): String {
-        val publicKey: PublicKey = getPublicKey()
+        val byteArray: ByteArray = Base64.decode(data, Base64.DEFAULT)
         val cipher = Cipher.getInstance(TRANSFORMATION).apply {
-            init(Cipher.ENCRYPT_MODE, publicKey)
+            init(Cipher.ENCRYPT_MODE, getKey())
         }
-        val cipherData = cipher.doFinal(Base64.decode(data, Base64.DEFAULT))
-        return String(cipherData)
+        val cipherData = cipher.doFinal(byteArray)
+        return Base64.encodeToString(cipherData, Base64.DEFAULT)
     }
 
     fun decrypt(data: String): String {
-        val privateKey: PrivateKey = getPrivateKey()
+        val byteArray: ByteArray = Base64.decode(data, Base64.DEFAULT)
         val cipher = Cipher.getInstance(TRANSFORMATION).apply {
-            init(Cipher.DECRYPT_MODE, privateKey)
+            init(Cipher.DECRYPT_MODE, getKey())
         }
-        val cipherData = cipher.doFinal(Base64.decode(data, Base64.DEFAULT))
-        return String(cipherData)
+        return String(cipher.doFinal(byteArray), Charsets.UTF_8)
     }
 
 }
