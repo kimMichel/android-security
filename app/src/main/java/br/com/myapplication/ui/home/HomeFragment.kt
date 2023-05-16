b581ed04bf141aa807105e6a2433a0f2bd828452
Michel Kim <kim98.michel@gmail.com>
2023-05-15 21:30:05 -0300

    Add Toast to now if password was stored

--- app/src/main/java/br/com/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/myapplication/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -47,6 +48,7 @@ class HomeFragment : Fragment() {
         }
         btnSave.setOnClickListener {
             viewModel.insert(textResult.text as String)
+            Toast.makeText(requireContext(), "Senha salva criptografada com sucesso!", Toast.LENGTH_SHORT).show()
         }
         btnStore.setOnClickListener {
             HomeFragmentDirections.actionHomeFragmentToStoreFragment().apply {
